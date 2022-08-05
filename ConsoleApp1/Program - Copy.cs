a6cde7d9f5a4fd94d94733d1c886a7f1b209f2b2
Chen An <v-chenan1@microsoft.com>
2022-08-05 05:50:00 +0000

    5

--- ConsoleApp1/Program - Copy.cs
+++ ConsoleApp1/Program - Copy.cs
@@ -1,14 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-//123f
-//789456
-//456123
-//0803
-//0805
-//mmm
-//mmm
-//1234
-//123
-//0805
-//mmm
-//11sd
